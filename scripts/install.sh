548c87fc0f31c649c3b423c466e39faf3471221b
Hazel <gildedhazel@gmail.com>
2026-01-03 16:50:52 -0600

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,8 @@
 # BEFORE YOU RUN THIS, CHANGE ../nixos/configuration.nix TO SHOW INSTALLED NIX RELEASE
 # DO NOT CHANGE IT IN THE GITHUB REPO NAME LOCATED IN ../flake.nix
 
-sudo nixos-rebuild switch --flake .. &
+sudo cp /etc/nixos/hardware-configuration.nix ../nixos/
+sudo nixos-rebuild switch --flake ..
 for config in $(ls ../config)
 do
   stow -t $HOME ../config/$config
