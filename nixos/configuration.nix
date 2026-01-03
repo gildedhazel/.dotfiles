b14c6c02031ce6594eddaa7cb0cef5237c09cc5a
Hazel <gildedhazel@gmail.com>
2026-01-03 08:27:42 -0600

    Re-ordered and added comments to configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,7 @@
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
   boot.loader.systemd-boot.enable = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
@@ -65,88 +65,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.hazel = {
-    description = "Hazel";
-    isNormalUser = true;
-    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
-    home = "/home/hazel";
-    packages = with pkgs; [
-      tree
-      neovim
-    ];
-  };
-
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    git
-    kitty
-    dysk
-    qutebrowser
-    chromium
-    waybar
-    rofi
-    pavucontrol
-    helvum
-    asdf-vm
-    widevine-cdm
-    discord
-    gnumake
-    pkg-config
-
-    # Screenshots
-    grimblast
-    jq
-    wl-clipboard-rs
-    grim
-    slurp
-
-    # DWL Dependencies
-    libinput
-    libxcb
-    libxkbcommon
-    pixman
-    wayland
-    wayland-protocols
-    wlroots_0_18
-    libx11
-    #xcbutilwm
-    xwayland
-    wayland-scanner
-    swaybg
-    eww
-    wlr-randr
-    pamixer
-    amdgpu_top
-    playerctl
-
-    # Rust package manager
-    cargo
-
-    # Neovim Dependencies
-    gcc
-    unzip
-    python3
-    # (import ./dwlmsg/dwlmsg-build.nix {})
-
-    (pkgs.wrapOBS {
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-vaapi
-      ];
-    })
-    xdg-desktop-portal-wlr
-    vlc
-    vesktop
-    stow
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -156,7 +74,6 @@
   # };
 
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -191,37 +108,157 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # ---------------------------------------------------------------------------------------------------------------------------
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hazel = {
+    description = "Hazel";
+    isNormalUser = true;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    home = "/home/hazel";
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+
+    # My preferred terminal
+    kitty
+
+    # Shows disk space per mounted partition in terminal
+    dysk
+
+    # Vim-like web browser
+    qutebrowser
+
+    # Configure sound devices
+    pavucontrol
+    helvum
+    pamixer
+
+    # Package manager used for installing proton-ge
+    asdf-vm
+
+    # Allow CDM content in qutebrowser and chromium browsers
+    widevine-cdm
+
+    # Make (for building programs)
+    gnumake
+
+    # Make dependency
+    pkg-config
+
+    # Screenshots
+    jq
+    wl-clipboard-rs
+    grim
+    slurp
+
+    # DWL dependencies
+    libinput
+    libxcb
+    libxkbcommon
+    pixman
+    wayland
+    wayland-protocols
+    wlroots_0_18
+    libx11
+    xwayland
+    wayland-scanner
+    swaybg
+
+    # Status bar widget
+    eww
+
+    # Application launcher
+    rofi
+
+    # Allows for easy sh scriptable monitor config
+    wlr-randr
+    amdgpu_top
+    playerctl
+
+    # Rust package manager
+    cargo
+
+    # Neovim plugin dependencies
+    gcc
+    unzip
+    python3
+
+    # OBS Studio
+    (pkgs.wrapOBS {
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-vaapi
+      ];
+    })
+
+    # Media
+    vlc
+
+    # Discord Client
+    vesktop
+
+    # Dotfiles Manager
+    stow
+  ];
+
+  # Hack Nerd Font
   fonts.packages = with pkgs; [
     nerd-fonts.hack
   ];
+
+  # Neovim
   programs.neovim = {
     enable = true;
-    withPython3 = true;
   };
-  programs.zsh.enable = true;
+  # Allows Mason to install a few Neovim LSPs/Formatters
   programs.npm.enable = true;
+
+  # I prefer zsh over bash or fish
+  programs.zsh.enable = true;
+
+  # Installs mesa for AMD GPUs
   hardware.graphics.enable = true;
-  programs.hyprland = {
-    enable = true;
-    withUWSM = false;
-    xwayland.enable = true;
-  };
+
+  # If you want Hyprland instead of DWL
+  # programs.hyprland = {
+  #   enable = true;
+  #   withUWSM = false;
+  #   xwayland.enable = true;
+  # };
+
+  # DWL is my preferred WM, and it's pulling from a local directory with my custom config
   programs.dwl = {
     enable = true;
     package = pkgs.dwl.overrideAttrs {
       src = ../dwl;
     };
   };
+
+  # Login scren; calls dwl-startup.sh script which sets everything up
   services.greetd = {
     enable = true;
     settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet -c 'dwl -s ~/.dotfiles/scripts/dwl-startup.sh'";
   };
+
+  # Steam...enough said
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
   };
+
+  # You have to have this for specific programs. If you're missing this portion, it'll tell you when you go to rebuild
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "steam"
@@ -229,6 +266,5 @@
       "steam-unwrapped"
       "steam-run"
       "widevine-cdm"
-      "discord"
     ];
 }
