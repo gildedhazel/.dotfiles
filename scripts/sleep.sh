5463dff44a3802ae0c13f675ab754a5d052a1ba2
Hazel <gildedhazel@gmail.com>
2026-01-06 05:08:50 -0600

    Changed display sleep method to dpms-off

--- scripts/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-swayidle timeout 60 "wlr-randr --output DP-1 --off; \
-                     wlr-randr --output DP-2 --off; \
-		     wlr-randr --output HDMI-A-2 --off" \
-	 resume "wlr-randr --output DP-1 --on; \
-	         wlr-randr --output DP-2 --on; \
-		 wlr-randr --output HDMI-A-2 --on; \
-		 ./monitors.sh"
