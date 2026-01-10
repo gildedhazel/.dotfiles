9eecac837199dd7afed8e8b68c52872e351f0751
Hazel <gildedhazel@gmail.com>
2026-01-10 16:55:52 -0600

    Honestly just check the filees that were changed; a few things have shifted

--- config/eww/.config/eww/scripts/dwl-workspaces-dp-2.sh
+++ config/eww/.config/eww/scripts/dwl-workspaces-dp-2.sh
@@ -3,25 +3,26 @@
 DWLMSG="$HOME/.local/bin/dwlmsg"
 MONITOR="DP-2"
 
-print_workspaces() {
-  buf=""
-  while IFS= read -r line; do
-    set -- $line
-    tag_num=$2
-    state=$3
+buf=""
+while IFS= read -r line; do
+  set -- $line
+  tag=$1
+  tag_num=$2
+  state=$3
 
-    if [ "$state" -eq 1 ]; then
-      icon=""
-      class="focused"
-    else
-      icon="○"
-      class="empty"
-    fi
-    buf="$buf (eventbox :cursor \"hand\" (button :class \"$class\" :onclick \"$DWLMSG -s -t $tag_num\" \"$icon\"))"
-  done < <( "$DWLMSG" -o "$MONITOR" -g -t | grep 'tag ')
-  echo "(box :halign \"center\" :valign \"center\" :vexpand true :hexpand true $buf)"
-}
+  # When we see "tags", it's the start of a NEW update
+  if [ "$tag" = "tags" ]; then
+    # Print the previous update's buffer (if exists)
+    [ -n "$buf" ] && echo "$buf"
+    buf=""
+    continue
+  fi
 
-while IFS= read -r _; do
-  print_workspaces
+  # Process workspace state
+  if [ "$state" -eq 1 ]; then
+    icon=""
+  else
+    icon="○"
+  fi
+  buf+=" $icon"
 done < <("$DWLMSG" -o "$MONITOR" -w -t)
--- config/eww/.config/eww/scripts/dwl-workspaces.sh
+++ config/eww/.config/eww/scripts/dwl-workspaces.sh
@@ -1,25 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+
+DWLMSG="$HOME/.local/bin/dwlmsg"
+MONITOR="DP-2"
 
 print_workspaces() {
   buf=""
-  while read -r line; do
-    tag_num=$(echo $line | awk '{ print $3 }')
-    tag_test=$(echo $line | awk '{ print $4 }')
-    if [ $tag_test -eq "1" ]; then
-      ws=$tag_num
+  while IFS= read -r line; do
+    set -- $line
+    tag_num=$2
+    state=$3
+
+    if [ "$state" -eq 1 ]; then
       icon=""
       class="focused"
     else
-      ws=$tag_num
       icon="○"
       class="empty"
-    fi  
-
-    buf="$buf (eventbox :cursor \"hand\" (button :class \"$class\" :onclick \"dwlmsg -s -t $ws\" \"$icon\"))"
-  done < <(dwlmsg -g -t | grep 'tag ')
+    fi
+    buf="$buf (eventbox :cursor \"hand\" (button :class \"$class\" :onclick \"$DWLMSG -s -t $tag_num\" \"$icon\"))"
+  done < <( "$DWLMSG" -o "$MONITOR" -g -t | grep -v 'tags')
   echo "(box :halign \"center\" :valign \"center\" :vexpand true :hexpand true $buf)"
 }
-while read -r _
-do
+while IFS= read -r  output; do
   print_workspaces
-done < <(dwlmsg -w -t)
+done < <("$DWLMSG" -o "$MONITOR" -w -t)
