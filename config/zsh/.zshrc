6d908bfcd53cd534ed876dfeab2a8571522fb77f
Hazel <gildedhazel@gmail.com>
2026-01-29 05:15:49 -0600

    Updated neovim to use nixd and nixfmt, plus other small changes

--- config/zsh/.zshrc
+++ config/zsh/.zshrc
@@ -143,6 +143,7 @@ alias kf="killall -9 GameThread"
 alias km="killall -9 java; killall -9 minecraft-launcher"
 alias gpt='f() { ollama run gpt-oss:20b $* | glow --width 0 };f'
 alias rebuild="sudo nixos-rebuild switch --flake ."
+alias reww="eww close-all; eww open-many bar bar1 bar2"
 export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.local/lib/hyde:$PATH"
 export VKBASALT_CONFIG_FILE=$HOME/.dotfiles/gaming/shaders/vkBasalt.conf
 export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,10 +1,46 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{ config, pkgs, ... }:
+let
+  tmux-everforest = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-everforest";
+    version = "";
+    src = pkgs.fetchFromGitHub {
+      owner = "TanglingTreats";
+      repo = "tmux-everforest";
+      rev = "b709e43";
+      # tag = "b709e43";
+      sha256 = "sha256-LQovIDU+dC0O7hf8cc/9Wfd7eVLeMmoxWd9N/VjFMSA=";
+    };
+  };
+in {
   home.username = "hazel";
   home.homeDirectory = "/home/hazel";
+
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = "Rose Pine Cursor";
+      package = pkgs.rose-pine-cursor;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs; [ tmuxPlugins.resurrect tmux-everforest ];
+    clock24 = true;
+    keyMode = "vi";
+    escapeTime = 10;
+    extraConfig = "";
+  };
 
   home.stateVersion = "25.11";
   programs.home-manager.enable = true;
